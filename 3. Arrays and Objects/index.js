63dbb5abc14498a824265e5b314a91a529ef6cef
joshdamp <joshuadamps@gmail.com>
2024-01-15 02:13:40 +0800

    Updated Arrays and Object

--- 3. Arrays and Objects/index.js
+++ 3. Arrays and Objects/index.js
@@ -38,6 +38,14 @@ console.log(average)
 // Checkpoint 3.1 How would you compare lists in python vs arrays in JavaScript
 // Answer: 
 
+/*
+Structure: Both Python lists and JavaScript arrays are collections of 
+elements and can be indexed, sliced, and iterated through12.
+Mutability: Both are mutable, meaning you can modify their elements31.
+Dynamic Size: Python lists and JavaScript arrays can grow or shrink dynamically3.
+Data Types: Both can store elements of different types34.
+Methods: JavaScript arrays can contain methods (functions), while Python lists cannot3.
+*/
 
 // Todo 3.3 Declare an object with information about IT114L (course code, name, units, number of students)
 // Your code here
@@ -45,7 +53,7 @@ let info = new Object()
 info = {
     course: "IT114L",
     name: "Joshua",
-    Units: "16",
+    Units: "1",
     NumStud: "35"
 }
 console.log(info)
@@ -59,16 +67,71 @@ console.log(info.professor)
 // Todo 3.5 Declare and array of objects with information about the courses you are taking this term
 // Your code here
 
+let courses = [
+    {
+        courseCode: "IT114L",
+        courseName: "Web Development",
+        units: 1,
+        professor: "Job Lipat",
+        numberOfStudents: 35
+    },
+    {
+        courseCode: "CS107L",
+        courseName: "Information Management Laboratory",
+        units: 3,
+        professor: "Adomar Ilao",
+        numberOfStudents: 30
+    },
+    {
+        courseCode: "CS107",
+        courseName: "Information Management ",
+        units: 3,
+        professor: "Dahlia De Mesa",
+        numberOfStudents: 40
+    }
+];
+
+console.log(courses);
 
 
 // Todo 3.5 Calculate the total number of units you are taking this term using the array of objects.
 // Your code here
 
+let totalUnits = 0;
+for (let i = 0; i < courses.length; i++) {
+    totalUnits += courses[i].units;
+}
+console.log("Total Units: " + totalUnits);
+
+
 // Checkpoint 3.2 What would be the equivalent of objects in python? How would you compare them?
 // Answer: 
+
+/*
+Structure: Both JavaScript objects and Python dictionaries are collections of key-value pairs. 
+They allow you to store and access data using keys.
+
+Accessing Values: In both JavaScript and Python, you can access values using the keys.
+
+Mutability: Both JavaScript objects and Python dictionaries are mutable. 
+This means you can add, modify, or delete key-value pairs after the object or dictionary is created.
+
+There are some differences:
+
+Methods: JavaScript objects can contain methods (functions), while Python dictionaries cannot.
+
+Key Types: In JavaScript, object keys are always strings. In Python, dictionary keys can be of any 
+immutable type, such as integers, floats, strings, and tuples.
+*/
 
 // Todo 3.6 Going back to the array of numbers, use the spread syntax to create a copy of the array with an additional number
 // Your code here
 
-// Todo 3.7 Going back to your IT114L object, extract the course code and units using the spread operator
+let newNumbers = [...numbers, 143];
+console.log(newNumbers);
+
+// Todo 3.7 Going back to your IT114L object, extract the course code and units using object deconstructing
 // Your code here
+
+let {course, Units} = info;
+console.log(course, Units);
